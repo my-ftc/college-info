8b7ae4c9e1c4de5c86f8f3fa405cfbaf42aa3764
ahuja-hemendra6 <ahuja.hemendra6@gmail.com>
2024-10-13 12:31:30 +0530

    Move the KollegeAI logo to the left

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,11 @@ const Header: React.FC<HeaderProps> = ({ onStartNew }) => {
 
   return (
     <header className="flex justify-between items-center w-full p-4 bg-gray-100 shadow-sm">
-      <div className="relative group ml-4">
+      <h1 className="text-2xl font-extrabold flex justify-center font-latoSemiBold ml-10">
+        KollegeAI
+      </h1>
+
+      <div className="relative group mr-4">
         <button
           className="p-1 rounded-full hover:bg-gray-200 transition-all duration-200 transform group-hover:scale-125"
           onMouseEnter={handleMouseEnter}
@@ -34,17 +38,11 @@ const Header: React.FC<HeaderProps> = ({ onStartNew }) => {
         </button>
 
         {showTooltip && (
-          <span className="absolute left-1/2 top-full mt-2 transform -translate-x-1/2 opacity-100 bg-cyan-700 text-white text-xs rounded py-1 px-2 transition-opacity duration-200">
+          <span className="absolute right-1/2 top-full mt-2 transform translate-x-1/2 opacity-100 bg-cyan-700 text-white text-xs rounded py-1 px-2 transition-opacity duration-200">
             New Chat
           </span>
         )}
       </div>
-
-      <h1 className="text-2xl font-extrabold flex justify-center font-latoSemiBold">
-        KollegeAI
-      </h1>
-
-      <div className="w-16"></div>
     </header>
   );
 };
