84d53ea6a03f8066533e7e4562949d36c996bbc8
Ninad Shenoy <shenoyninad@gmail.com>
2024-10-25 20:57:26 +0530

    Mobile UI (#32)
    * Implements responsive UI for the app
    
    * Reduces the size of the logos
    
    * UI changes for mobile view
    
    * Fixes the build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,16 +14,32 @@ export default function Home() {
     "Looking at MBA/PGDM admissions?",
     "Get all your answers here. Instantly.",
   ];
-  const [randomQuestions, setRandomQuestions] = useState<string[]>([]);
-  const [selectedQuestion, setSelectedQuestion] = useState<string | null>(null);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [threadId, setThreadId] = useState<string | null>(null);
   const logos = Array.from({ length: 20 }, (_, i) => `${i + 1}.png`);
+  const [randomQuestions, setRandomQuestions] = useState<string[]>([]);
+  const [selectedQuestion, setSelectedQuestion] = useState<string | null>(null);
 
   const openAI = new OpenAI({
     apiKey: process.env.NEXT_PUBLIC_CHATGPT_API_KEY!,
     dangerouslyAllowBrowser: true,
   });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 4) % logos.length);
+      }, 500); // Transition duration
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, [logos.length]);
+
+  const visibleLogos = logos
+    .slice(currentIndex, currentIndex + 4)
+    .concat(logos.slice(0, Math.max(0, currentIndex + 4 - logos.length)));
 
   useEffect(() => {
     const randomQuestionsArr: string[] = questionnaireData.map(
@@ -89,48 +105,85 @@ export default function Home() {
     setSearchQuery("");
   };
 
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 480px)");
+
+    const handleResize = () => {
+      setIsSmallScreen(mediaQuery.matches);
+    };
+
+    handleResize();
+    mediaQuery.addEventListener("change", handleResize);
+
+    return () => mediaQuery.removeEventListener("change", handleResize);
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen font-latoRegular">
       <Header onStartNew={handleStartNew} />
       <div className="flex lg:flex-row-reverse flex-grow w-full mt-6 h-full md:flex-col sm:flex-col xs:flex-col">
-        <div className="lg:w-[70%] md:w-full sm:w-full h-auto flex flex-col items-center lg:m-2 md:mt-5 sm:mt-5 lg:pt-0 md:pt-8 sm:pt-8 xs:pt-8 bg-[#F9F9F9] justify-center rounded-lg">
-          <b className="lg:text-xl md:text-xl sm:text-xl xs:text-sm">
-            Apply to our partnered universities
-          </b>
-          <div className="grid lg:grid-cols-5 sm:grid-cols-4 md:grid-cols-4 xs:grid-cols-5 gap-4 p-5">
-            {logos.map((logo, index) => {
-              const logoNumber = logo.split(".")[0];
-              const collegeName = imageCollegeMapping.find(
-                (college) => college.file === logoNumber
-              )?.college;
-
-              return (
-                <div
-                  key={index}
-                  className="flex justify-center items-center p-3 border border-gray-200 rounded"
-                >
-                  <img
-                    src={`/assets/college-logos/${logo}`}
-                    alt={`Logo ${index + 1}`}
-                    className="lg:h-20 lg:w-20 md:h-20 md:w-20 sm:h-20 sm:w-20 object-contain grayscale-0 cursor-pointer xs:h-10 xs:w-20"
-                    title={collegeName || "College Logo"}
-                    onClick={() => {
-                      if (collegeName) {
-                        setSelectedQuestion(null);
-                        setTimeout(() => {
-                          setSelectedQuestion(
-                            `Give me an overview of ${collegeName}`
-                          );
-                          handleSearchSubmit();
-                        }, 0);
-                      }
-                    }}
-                  />
-                </div>
-              );
-            })}
+        {isSmallScreen && (
+          <div className="flex flex-col justify-center items-center">
+            <b className="text-lg">Apply to our partnered universities</b>
+            <div className="grid lg:grid-cols-5 sm:grid-cols-12 sm:grid-rows-1 md:grid-cols-4 gap-4 p-5">
+              {/* Carousel Container */}
+              <div className="flex flex-row items-center space-x-4 overflow-x-auto scrollbar-hide snap-x snap-mandatory">
+                {visibleLogos.map((logo, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-row flex-shrink-0 w-20 h-20 p-4 justify-center items-center align-middle rounded-md snap-center"
+                  >
+                    <img
+                      src={`/assets/college-logos/${logo}`}
+                      alt={`Logo ${index + 1}`}
+                      className="h-20 w-20 object-contain grayscale-0 cursor-pointer"
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
+        )}
+        {!isSmallScreen && (
+          <div className="lg:w-[70%] md:w-full sm:w-full h-auto flex flex-col items-center lg:m-2 md:mt-5 sm:mt-5 lg:pt-0 md:pt-8 sm:pt-8 xs:pt-8 bg-[#F9F9F9] justify-center rounded-lg">
+            <b className="lg:text-xl md:text-xl sm:text-xl xs:text-sm">
+              Apply to our partnered universities
+            </b>
+            <div className="grid lg:grid-cols-5 sm:grid-cols-4 md:grid-cols-4 xs:grid-cols-5 gap-4 p-5">
+              {logos.map((logo, index) => {
+                const logoNumber = logo.split(".")[0];
+                const collegeName = imageCollegeMapping.find(
+                  (college) => college.file === logoNumber
+                )?.college;
+
+                return (
+                  <div
+                    key={index}
+                    className="flex justify-center items-center p-3 border border-gray-200 rounded"
+                  >
+                    <img
+                      src={`/assets/college-logos/${logo}`}
+                      alt={`Logo ${index + 1}`}
+                      className="lg:h-20 lg:w-20 md:h-20 md:w-20 sm:h-20 sm:w-20 object-contain grayscale-0 cursor-pointer xs:h-10 xs:w-20"
+                      title={collegeName || "College Logo"}
+                      onClick={() => {
+                        if (collegeName) {
+                          setSelectedQuestion(null);
+                          setTimeout(() => {
+                            setSelectedQuestion(
+                              `Give me an overview of ${collegeName}`
+                            );
+                            handleSearchSubmit();
+                          }, 0);
+                        }
+                      }}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
         <div className="lg:w-[130%] md:w-full sm:w-full max-h-screen flex flex-col justify-center items-center lg:m-4 md:m-0 sm:m-0 mb-10">
           {!selectedQuestion ? (
             <div className="mb-10">
@@ -142,7 +195,9 @@ export default function Home() {
                     className="bg-white hover:bg-[#F9F9F9] p-6 lg:w-[25%] md:w-[25%] sm:w-[75%] xs:w-[75%] rounded-lg text-center cursor-pointer border-2 lg:min-h-36 md:min-h-36 sm:min-h-36 xs:min-h-20 h-auto inline align-middle"
                     onClick={() => setSelectedQuestion(question)}
                   >
-                    <p className="font-bold">{question}</p>
+                    <p className="lg:font-bold md:font-bold sm:font-semibold xs:font-thin">
+                      {question}
+                    </p>
                   </div>
                 ))}
               </div>
